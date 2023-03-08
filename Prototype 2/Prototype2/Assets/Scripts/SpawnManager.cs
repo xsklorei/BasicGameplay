de4b9a3f52c42c928b40556533f9b95accd9e781
xsklorei <jcvxka@gmail.com>
2023-03-08 11:38:36 -0800

    added script
    went to 2.4 step 5

--- Prototype 2/Prototype2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Prototype2/Assets/Scripts/SpawnManager.cs
@@ -8,28 +8,27 @@ public class SpawnManager : MonoBehaviour
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
 
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            //generate a random animal and spawn point
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        }
+        
     }
 
     void SpawnRandomAnimal()
     {
-
+        //generate a random animal and spawn point
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 
 }
