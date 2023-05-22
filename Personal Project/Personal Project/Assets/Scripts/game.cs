bc4a339a792927495ed0912836cb0b44d054b0c1
Ashley Schmidt <jcvxka@gmail.com>
2023-05-22 11:39:59 -0700

    aDDED test script

---
+++ Personal Project/Personal Project/Assets/Scripts/game.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class game : MonoBehaviour
+{
+
+    public int health;
+    public int numofHearts;
+
+    public Image[] Hearts;
+    public Sprite fullHearts;
+    public Sprite emptyHearts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
